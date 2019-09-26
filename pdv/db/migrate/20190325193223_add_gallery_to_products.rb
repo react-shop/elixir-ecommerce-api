30636e9a81880d83bb279b1fa1654bbeb19bae85
Vinicius Arruda <viniarruda.souza@gmail.com>
2019-09-26 16:41:55 -0300

    feat: add structure using nest

--- pdv/db/migrate/20190325193223_add_gallery_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddGalleryToProducts < ActiveRecord::Migration[5.2]
-  def change
-  	add_column :products, :gallery, :string
-  end
-end
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+}
